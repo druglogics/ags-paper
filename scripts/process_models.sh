2ca9408d1d339f781de118e645ff923af70e16a3
john <bblodfon@gmail.com>
2020-04-24 19:29:00 +0200

    remove hack
    Remove part of the script that was adding the substring '_run_'
    to the model names and simplified documentation.
    This issue was fixed in drabme#05a69be commit.

--- scripts/process_models.sh
+++ scripts/process_models.sh
@@ -9,9 +9,5 @@ for file in ${files}; do
   then
     rm $file
   else
-    # change modelname inside file to mimic gitsbe generated modelnames,
-    # otherwise drabme will fail to recognise it!
-    # Remember to change 'cascade_1_0' to 'cascade_2_0' if appropriate
-    sed -i 's/modelname: cascade_1_0_/modelname: cascade_1_0_run_/' $file
   fi
 done
