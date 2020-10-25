590fc944aaea21c8c1f75e79b81c04f888909a15
john <bblodfon@gmail.com>
2020-10-25 20:25:07 +0100

    add script to run druglogics-synergy on the scrambled topology CASCADE 1.0 files

---
+++ scripts/run_druglogics_synergy_on_scrambled_topologies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Run this script from the `druglogics-synergy` repository root (v1.2.0)
+# The `scrambled_topologies` directory is produced via the `gen_scrambled_topologies.R`
+# script and should also be placed in the repository root
+
+training_files=`ls scrambled_topologies`
+files_num=`ls scrambled_topologies | wc -l`
+count=0
+
+# change drabme config option (use bliss synergy assessement)
+# all other option to default values in v1.2.0
+sed -i 's/synergy_method:.*/synergy_method:\tbliss/' ags_cascade_1.0/config
+
+for file in ${training_files}; do
+  count=$((count + 1))
+  echo Using topology file No. $count/$files_num: $file
+  cat scrambled_topologies/$file > ags_cascade_1.0/network.sif
+
+  # strip .sif extension for project naming
+  file_name=$(echo "$file" | sed 's/\.[^.]*$//')
+
+  start=`date +%s`
+
+  # run calibrated models simulations
+  cat ags_cascade_1.0/steadystate > ags_cascade_1.0/training
+  java -cp target/synergy-1.2.0-jar-with-dependencies.jar eu.druglogics.synergy.Launcher --inputDir=ags_cascade_1.0 --project=${file_name}_ss > /dev/null 2>&1
+
+  # run random proliferative models simulations
+  cat ags_cascade_1.0/random_train > ags_cascade_1.0/training
+  java -cp target/synergy-1.2.0-jar-with-dependencies.jar eu.druglogics.synergy.Launcher --inputDir=ags_cascade_1.0 --project=${file_name}_random > /dev/null 2>&1
+
+  runtime=$(($(date +%s)-$start))
+  echo Execution Time: "$(($runtime / 60)) minutes"
+done
+
