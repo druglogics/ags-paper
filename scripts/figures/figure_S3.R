75a9d5ad9b9b4542b96932f132f599549cd5db16
john <bblodfon@gmail.com>
2021-11-27 13:09:36 +0100

    add figure S3 script and outputs

---
+++ scripts/figures/figure_S3.R
@@ -0,0 +1,37 @@
+library(ggpubr)
+library(ggplot2)
+library(scales)
+
+# see `scripts/fit_vs_perf_cascade1_lo.R`
+res = readRDS(file = "data/res_fit_aucs_cascade1.rds")
+
+# data is not normally distributed
+shapiro.test(x = res$roc_auc)
+shapiro.test(x = res$pr_auc)
+shapiro.test(x = res$avg_fit)
+
+# Figure 3S - ROC
+p = ggpubr::ggscatter(data = res, x = "avg_fit", y = "roc_auc", color = "per_flipped_data",
+  xlab = "Average Fitness per Model Ensemble",
+  title = "Fitness to AGS Steady State vs Performance (ROC)",
+  ylab = "ROC AUC", add = "reg.line", conf.int = TRUE,
+  add.params = list(color = "blue", fill = "lightgray"),
+  cor.coef = TRUE, cor.coeff.args = list(method = "kendall",
+    label.y.npc = "bottom", size = 6, cor.coef.name = "tau")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_distiller(n.breaks = 5, labels = scales::label_percent(accuracy = 1L), palette = 'RdYlBu', guide = guide_colourbar(title = '%Data Flipped'))
+ggpubr::ggpar(p, legend = "right", font.legend = 14)
+ggplot2::ggsave(filename = 'scripts/figures/figure_3S_ROC.pdf', width = 7, height = 5, device = cairo_pdf)
+
+# Figure 3S - PR
+p = ggpubr::ggscatter(data = res, x = "avg_fit", y = "pr_auc", color = "per_flipped_data",
+  xlab = "Average Fitness per Model Ensemble",
+  title = "Fitness to AGS Steady State vs Performance (Precision-Recall)",
+  add.params = list(color = "blue", fill = "lightgray"),
+  ylab = "PR AUC", add = "reg.line", conf.int = TRUE,
+  cor.coef = TRUE, cor.coeff.args = list(method = "kendall",
+    size = 6, cor.coef.name = "tau")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_distiller(n.breaks = 5, labels = scales::label_percent(accuracy = 1L), palette = 'RdYlBu', guide = guide_colourbar(title = '%Data Flipped'))
+ggpubr::ggpar(p, legend = "right", font.legend = 14)
+ggplot2::ggsave(filename = 'scripts/figures/figure_3S_PR.pdf', width = 7, height = 5, device = cairo_pdf)
