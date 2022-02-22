076e10b02c8acb8d938147e11cd4846ed574f6ee
john <bblodfon@gmail.com>
2022-02-22 21:43:01 +0100

    renaming figures 9 and S7 according the publication

--- scripts/figures/figure_9_S7.R
+++ scripts/figures/figure_9_S7.R
@@ -16,7 +16,7 @@ stat_test_roc = res %>%
   rstatix::wilcox_test(formula = roc_auc ~ param, comparisons = param_comp) %>%
   rstatix::add_significance("p")
 
-# ROC AUCs
+# ROC AUCs (Figure S7)
 roc_fig = ggpubr::ggboxplot(res, x = "param", y = "roc_auc", fill = "param", palette = "Set1",
   add = "jitter", xlab = "", ylab = "ROC AUC") +
   scale_x_discrete(breaks = c("link-only","topology-only"),
@@ -28,7 +28,7 @@ roc_fig = ggpubr::ggboxplot(res, x = "param", y = "roc_auc", fill = "param", pal
   geom_hline(yintercept = 0.5, linetype = 'dashed', color = "red") +
   geom_text(aes(x = 2.1, y = 0.45, label="Random Predictions (AUC = 0.5)", size = 5)) +
   theme(legend.position = "none")
-ggplot2::ggsave(filename = 'scripts/figures/figure_9_ROC.pdf', width = 7, height = 5, device = cairo_pdf)
+ggplot2::ggsave(filename = 'scripts/figures/figure_S7.pdf', width = 7, height = 5, device = cairo_pdf)
 
 stat_test_pr = res %>%
   rstatix::wilcox_test(formula = pr_auc ~ param, comparisons = param_comp) %>%
@@ -46,8 +46,8 @@ pr_fig = ggboxplot(res, x = "param", y = "pr_auc", fill = "param", palette = "Se
   geom_hline(yintercept = 6/153, linetype = 'dashed', color = "red") +
   geom_text(aes(x = 1.9, y = 0.08, label="Random Predictions (AUC = 0.04)"), size = 5) +
   theme(legend.position = "none")
-ggplot2::ggsave(filename = 'scripts/figures/figure_9_PR.pdf', width = 7, height = 5, device = cairo_pdf)
+ggplot2::ggsave(filename = 'scripts/figures/figure_9.pdf', width = 7, height = 5, device = cairo_pdf)
 
 # Both plots in one
 ggpubr::ggarrange(roc_fig, pr_fig, labels = LETTERS[1:2], nrow = 1, ncol = 2)
-ggplot2::ggsave(filename = 'scripts/figures/figure_9.pdf', width = 14, height = 5, device = cairo_pdf)
+ggplot2::ggsave(filename = 'scripts/figures/figure_9_ROC_and_PR.pdf', width = 14, height = 5, device = cairo_pdf)
